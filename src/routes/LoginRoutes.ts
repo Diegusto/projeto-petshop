4df6ae7b96404213b478041fb9e85146e9964de9
Diegusto <diegusto35@gmail.com>
2022-11-17 17:07:05 -0300

    refresh token

--- src/routes/LoginRoutes.ts
+++ src/routes/LoginRoutes.ts
@@ -3,6 +3,7 @@ import { UsersRepository } from "../repositories/usersRepository";
 import {sign} from "jsonwebtoken";
 import { compare } from "bcryptjs";
 import { AppError } from "../AppError";
+import { verify } from "jsonwebtoken";
 
 const loginRouter = Router();
 
@@ -30,14 +31,58 @@ loginRouter.post('/', async (request,response) =>{
 
     const secrect = process.env.TOKEN_KEY
 
+    const refresh = process.env.REFRESH_KEY
+
     if (!secrect){
+        throw new AppError('user not found', 401)
+    }
+
+    if (!refresh){
         throw new AppError('user not found', 401)
     }
 
     const token = sign({}, secrect, {
         subject: findUser.id,
-        expiresIn: "2d"
+        expiresIn: "1d"
     })
+
+    const refreshToken = sign({}, refresh,{
+        subject:findUser.id,
+        expiresIn: "10d"
+    })
+
+    return response.status(200).json({token, refreshToken})
+
+})
+
+loginRouter.post('/refresh', async (request,response) =>{
+    
+    const authHeader = request.headers.authorization;
+    const refToken = process.env.REFRESH_KEY
+    const secret = process.env.TOKEN_KEY
+
+    if (!authHeader){
+        throw new AppError('refresh token is missing',401)
+    }
+
+    if (!refToken){
+        throw new AppError('invalid token',401)
+    }
+
+    if (!secret){
+        throw new AppError('invalid token',401)
+    }
+
+    const [, refresh] = authHeader.split(' ');
+    console.log(refresh)
+
+    try {
+        const tokenDecoded = verify(refresh, refToken);
+    } catch {
+        throw new AppError('invalid token', 401)
+    }
+
+    const [, token] = authHeader.split (' ');
 
     return response.status(200).json(token)
 
